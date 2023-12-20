3ad8291fceabacb9bf8993b53c761c976c3a8111
Rizqi H <rizqh@protonmail.ch>
2023-12-20 17:32:09 +0700

    update

--- lib/screen/movie_detail.dart
+++ lib/screen/movie_detail.dart
@@ -8,6 +8,9 @@ class MovieDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String path;
+    double screenHeight =
+        MediaQuery.of(context).size.height; //ambil tinggi layar
+
     if (selectedMovie.posterPath != null) {
       path = 'https://image.tmdb.org/t/p/w500/${selectedMovie.posterPath}';
     } else {
@@ -19,11 +22,19 @@ class MovieDetail extends StatelessWidget {
         appBar: AppBar(
           title: Text('${selectedMovie.title}'),
         ),
-        body: Column(
+        body: SingleChildScrollView(
+            //menambahkan fitur scroll
+            child: Center(
+                //menempatkan widget di tengah
+                child: Column(
           children: <Widget>[
-            Image.network(path),
+            Container(
+              padding: EdgeInsets.all(16), //menambahkan padding
+              height: screenHeight / 1.5,
+              child: Image.network(path),
+            ),
             Text('${selectedMovie.overview}'),
           ],
-        ));
+        ))));
   }
 }
